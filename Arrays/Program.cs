b285efbfd4b1e5ba53e43bf43ddf95908c5ccfb3
ibrahimyyildirim <ibrahimyldrm@outlook.com>
2021-09-07 15:55:13 +0300

    Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[2];
+            students[0] = "İbrahim";
+            students[1] = "Yıldırım";
+
+            string[] students2 = { "İbrahim", "Yıldırım" };
+
+            foreach (var student in students2)
+            {
+                Console.WriteLine(student);
+            }
+
+            //multidimensional arrays
+            string[,] regions = new String[2, 3]
+            {
+                {"İstanbul","Kocaeli","Edirne"},
+                {"İzmir","Manisa","Aydın"}
+            };
+
+            // GetUpperBound arrayin satir ve sutununun en ust degerini alir
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++) 
+                {
+                    Console.WriteLine(regions[i,j]);
+                }
+                Console.WriteLine("***");
+            }
+            Console.ReadLine();
+        }
+    }
+}
