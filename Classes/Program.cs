d049798c7d8ab111853621b778d5bcad4cad196b
ibrahimyyildirim <ibrahimyldrm@outlook.com>
2021-09-07 21:38:18 +0300

    Classes

---
+++ Classes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Delete();
+
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Ahmet",
+                LastName = "Ak",
+                City = "İstanbul"
+            };
+
+            Console.WriteLine(customer.City); 
+
+            Console.ReadLine();
+        }
+    }
+}
