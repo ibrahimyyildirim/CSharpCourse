6a2ff9e84301be0d18842f5e9bc7a44da4a6b76c
ibrahimyyildirim <ibrahimyldrm@outlook.com>
2021-09-11 14:41:06 +0300

    RecapDemo

---
+++ RecapDemo/CustomerManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecapDemo
+{
+    class CustomerManager
+    {
+        //property injection
+        public ILogger Logger { get; set; }
+        public void Add()
+        {
+            Logger.Log();
+            Console.WriteLine("Customer added!");
+        }
+    }
+}
---
+++ RecapDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecapDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Logger = new FileLogger();
+            customerManager.Add();
+
+            Console.ReadLine();
+        }
+    }
+}
