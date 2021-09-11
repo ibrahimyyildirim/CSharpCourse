e13dd33c467938b78bb5bff8c31fadbd20abfae4
ibrahimyyildirim <ibrahimyldrm@outlook.com>
2021-09-11 22:00:41 +0300

    AccessModifiers

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    /*Private: property tanımladığımızda default'u 'private'tır. Sadece tanımlandığı blok içinde geçerlidir.
+
+    Protected: private'ın tüm özelliğini kapsar.
+    Private'dan tek farkı tanımlanan method, property vs. intherite edildiği sınıflarda da kullanılabilir.
+
+    Internal: class'ın default'u internal'dır.
+    Bağlı bulunduğu projede referans ihtiyacı olmadan kullanılabilir.
+    Üst classlar private ve protected olamaz ! Ancak iç içe classlar kullanılır.
+
+    public: farklı bir projeden de kullanmak istediğimiz zaman kullanılır.*/
+    class Customer
+    {
+        protected int Id;
+    }
+
+    class Student : Customer
+    {
+        public void Save()
+        {
+            Student student = new Student()
+            {
+                Id = 1
+            };
+        }
+    }
+}
