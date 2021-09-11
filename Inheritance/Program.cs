1740155a0fb392dcceb88524df0139690af728a1
ibrahimyyildirim <ibrahimyldrm@outlook.com>
2021-09-11 13:56:20 +0300

    Inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[2]
+            {
+                new Customer()
+                {
+                    FirstName = "İbrahim"
+                },
+                new Student()
+                {
+                    FirstName = "Yıldırım"
+                }
+            };
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
