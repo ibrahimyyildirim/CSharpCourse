6f162764a9d2482d3076341f8e3076c15c4bb7cd
ibrahimyyildirim <ibrahimyldrm@outlook.com>
2021-09-19 19:45:04 +0300

    Exceptions

---
+++ Exceptions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                Find();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                
+            }
+            Console.ReadLine();
+        }
+
+        private static void Find()
+        {
+            List<string> students = new List<string> {"Ahmet", "Mehmet", "Ayşe"};
+
+            if (!students.Contains("Fatma"))
+            {
+                throw new RecordNotFoundException("Record not found");
+            }
+            else
+            {
+                Console.WriteLine("Record Found!");
+            }
+        }
+    }
+}
